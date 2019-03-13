34651b226a93f85b351ab69769863427cd0a7539
Gaetano Giacalone <ggiacalone@example.com>
2019-03-13 00:58:27 +0000

    added htdocs directory for bind mount

---
+++ htdocs/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Title Page</title>
+</head>
+<body>
+  <h1 align="center">It Totally Works!</h1>
+  <h2 align="center"><?php echo gethostname(); ?></h2>
+  <h3 align="center"> This is my new line for testing <?php echo date("Y"); ?></h3>
+  <?php phpinfo(); ?>
+</body>
+</html>
